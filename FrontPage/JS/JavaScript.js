4b3b0189a915757eb0dd49abae7bd7c0f15e6f1a
TrevorMicek <miceking@comcast.net>
2021-01-27 13:49:02 -0700

    commit on 'main'

--- FrontPage/JS/JavaScript.js
+++ FrontPage/JS/JavaScript.js
@@ -1,1 +1,20 @@
  
+function openDropDown() {
+    console.log('here')
+    document.getElementById("hamburger").style.display = "none";
+    document.getElementById("dropDownWrapper").style.display = "inline-block";
+    var navbar = document.getElementById("rightsideNav");
+    navbar.style.height = "75vh";
+    document.getElementById("listItems").style.display = "block";
+  
+   
+
+}
+function closeDropDown() {
+    document.getElementById("dropDownWrapper").style.display = "none";
+    document.getElementById("hamburger").style.display = "inline-block";
+    document.getElementById("rightsideNav").style.height = "auto"
+    document.getElementById("listItems").style.display = "none";
+
+
+}
